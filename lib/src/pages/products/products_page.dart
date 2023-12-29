8ee1f0cdadfb952dc77daef940c100753baa9501
jsmile <jsmile@naver.com>
2023-12-29 23:22:53 +0900

    riverpod inifite scroll & PagedChildBuilderDelegate()

--- lib/src/pages/products/products_page.dart
+++ lib/src/pages/products/products_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:riverpod_infinite_scroll/src/models/product.dart';
+import 'package:riverpod_infinite_scroll/src/pages/product/product_page.dart';
+import 'package:riverpod_infinite_scroll/src/repositories/product_repository.dart';
 
 class ProductsPage extends ConsumerStatefulWidget {
   const ProductsPage({super.key});
@@ -9,15 +13,120 @@ class ProductsPage extends ConsumerStatefulWidget {
 }
 
 class _ProductsPageState extends ConsumerState<ProductsPage> {
+  // page 변화 시 사용
+  final PagingController<int, Product> _pagingController =
+      PagingController(firstPageKey: 1);
+
+  @override
+  void initState() {
+    super.initState();
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchProducts(pageKey);
+    });
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Product List'),
       ),
-      body: const Center(
-        child: Text('Product List'),
+      body: RefreshIndicator(
+        onRefresh: () async => _pagingController.refresh(),
+        child: PagedListView<int, Product>.separated(
+          pagingController: _pagingController,
+          separatorBuilder: (context, index) => const Divider(),
+          builderDelegate: PagedChildBuilderDelegate(
+            itemBuilder: (context, product, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ProductPage(id: product.id),
+                    ),
+                  );
+                },
+                child: Row(
+                  children: [
+                    const SizedBox(width: 20.0),
+                    CircleAvatar(
+                      child: Text(product.id.toString()),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        title: Text(product.title),
+                        subtitle: Text(product.brand),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            firstPageErrorIndicatorBuilder: (context) => Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 50.0, horizontal: 30.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Something went wrong!',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    const SizedBox(height: 20.0),
+                    Text(
+                      '${_pagingController.error}',
+                      style: const TextStyle(fontSize: 18.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20.0),
+                    OutlinedButton(
+                      onPressed: () => _pagingController.refresh(),
+                      child: const Text(
+                        'Try again',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            noMoreItemsIndicatorBuilder: (context) => const Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  'No more products !',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  Future<void> _fetchProducts(int pageKey) async {
+    try {
+      final newProducts =
+          await ref.read(productRepositoryProvider).getProducts(pageKey);
+      final isLastPage = newProducts.length < limit;
+      if (isLastPage) {
+        // 마지막 페이지로 추가함.
+        _pagingController.appendLastPage(newProducts);
+      } else {
+        // 마지막 페이지가 아니면 다음 페이지로 추가함.
+        final nextPageKey = pageKey + 1; // page 번호를 이용하는 경우
+        // final nextPageKey = pageKey + newProducts.length;  // item 갯수를 이용하는 경우
+        _pagingController.appendPage(newProducts, nextPageKey);
+      }
+    } catch (err) {
+      _pagingController.error = err;
+    }
   }
 }
--- lib/src/repositories/product_repository.dart
+++ lib/src/repositories/product_repository.dart
@@ -6,9 +6,7 @@ import '../models/product.dart';
 
 part 'product_repository.g.dart';
 
-const limit = 10; // 10 products per page
-int totalProducts = 0;
-int totalPages = 1; // 총 상품갯수에 대한 페이지 수
+const limit = 20; // 10 products per page
 
 /// 상품 목록과 개별 상품을 가져오는 레포지토리
 class ProductRepository {
@@ -16,8 +14,7 @@ class ProductRepository {
 
   ProductRepository(this.dio);
 
-  Future<List<Product>> getProducts(int page,
-      {CancelToken? cancelToken}) async {
+  Future<List<Product>> getProducts(int page) async {
     try {
       final Response response = await dio.get(
         '/products',
@@ -25,16 +22,11 @@ class ProductRepository {
           'limit': limit,
           'skip': (page - 1) * limit,
         },
-        cancelToken: cancelToken, // 사용자의 취소 반영을 위한 cancelToken
       );
 
       if (response.statusCode != 200) throw 'Fail to fetch products';
 
       final List productList = response.data['produtcts'];
-
-      totalProducts = response.data['total'];
-      totalPages =
-          (totalProducts ~/ limit) + (totalProducts % limit > 0 ? 1 : 0);
 
       final products = [
         for (final product in productList) Product.fromJson(product)
